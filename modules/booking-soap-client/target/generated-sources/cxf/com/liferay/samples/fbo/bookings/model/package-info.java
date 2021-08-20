d397d0f31fc5f856be2dbdb13b92d9e93f1a696f
fabian-bouche-liferay <fabian.bouche@liferay.com>
2021-08-20 16:51:08 +0200

    removed target dirs

--- modules/booking-soap-client/target/generated-sources/cxf/com/liferay/samples/fbo/bookings/model/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://liferay.com/samples/fbo/bookings-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.liferay.samples.fbo.bookings.model;
